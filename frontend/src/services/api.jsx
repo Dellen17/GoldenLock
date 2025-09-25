28946b69266bf400cb77b155b639af6e434a449b
Dellen17 <gabrielkatara15@gmail.com>
2025-09-25 22:22:18 +0200

    Update api.jsx to log cookies and attempt manual Cookie header

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -7,7 +7,21 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    console.log('Request headers:', config.headers); // Debug log
+    const cookies = document.cookie.split('; ').reduce((acc, cookie) => {
+      const [name, value] = cookie.split('=');
+      acc[name] = value;
+      return acc;
+    }, {});
+    if (cookies.access_token) {
+      config.headers['Cookie'] = `access_token=${cookies.access_token}`;
+    }
+    console.log('Request config:', {
+      url: config.url,
+      method: config.method,
+      headers: config.headers,
+      withCredentials: config.withCredentials,
+      cookies: document.cookie
+    });
     return config;
   },
   (error) => {
@@ -20,6 +34,7 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
+      console.log('401 error intercepted, redirecting to /login');
       window.location.href = '/login';
     }
     return Promise.reject(error);
